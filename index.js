401f47d6a6fb6a9ca8d518bf8b82a1e1e79a0dfa
lolita89 <cika.lolita@hotmail.com>
2018-07-23 16:57:07 +0200

    vue5 vmodel, input changing the same time

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'Hi love!' 
-    },
-    methods: { 
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
     }
 });
